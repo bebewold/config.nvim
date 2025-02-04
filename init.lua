333eb840bcbc22d14300c93556ca5d670fa86d1e
Xavier Giguere <bebewold@gmail.com>
2025-02-03 21:58:09 -0500

    pull on startup

--- init.lua
+++ init.lua
@@ -1,25 +1,6 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- pull cong on startup
+if not os.execute 'git -C ~/.config/nvim/ pull > /dev/null' then
+  print 'Could not pull config...'
+end
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-
--- [[ Setting options ]]
-require 'options'
-
--- [[ Basic Keymaps ]]
-require 'keymaps'
-
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
-
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
-
-require 'custom.plugins'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require 'main'
