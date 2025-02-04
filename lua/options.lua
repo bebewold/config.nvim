6c4d4493f2955324b74f6c390554830c7664b7ca
Xavier Giguère <xavier.giguere@lcieducation.net>
2025-02-03 22:06:36 -0500

    change option

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -61,4 +61,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
 
--- vim: ts=2 sts=2 sw=2 et
+-- [[ Shell ]]
+-- Windows shell
+if (vim.loop.os_uname().sysname == 'Windows_NT') then
+  vim.o.shell = 'powershell.exe'
+end
